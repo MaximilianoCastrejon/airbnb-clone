07b575f510a731e509cbb955eb8f6fab45e739c9
Maximiliano Castrejon <mcastrejon.dev.2021@gmail.com>
2024-05-16 20:49:38 -0600

    reduced code in buildQuery with cleaner solutions. Not actually tested yet

--- server/libs/buildQuery.js
+++ server/libs/buildQuery.js
@@ -1,55 +1,40 @@
 export const buildQuery = async (
   collection,
-  { stringParams, numQuery, idFields },
+  query,
+  numericFilters,
   { projection, sort, pagination, populate }
 ) => {
   const queryObject = {};
-  if (stringParams) {
-    stringParams.forEach((obj) => {
-      Object.entries(obj).forEach(([key, value]) => {
-        console.log("[key, value]", [key, value]);
-        const match = key.match(/(.*)Options/);
-        if (match) {
-          const field = match[1];
-          queryObject[field] = { $regex: obj[field] };
-          if (key.endsWith("Options")) {
-            queryObject[field]["$options"] = value ? value : "";
-          }
-        }
-      });
-    });
-  }
 
-  // Give option for range queryObject.createdAt: { $gte: period.from, $lte: period.to }
-  if (numQuery) {
-    let filters = mapOperators(numQuery);
-    const from_fields = new Set();
-    filters.split(",").forEach((item) => {
-      const [field, operator, filterValue] = item.split("_");
-      if (numQuery.options.includes(field)) {
-        let castedFilterValue = castNumericValue(field, filterValue);
-        // If appeard before
-        if (from_fields.has(field)) {
-          if (field === "createdAt") {
-            castedFilterValue.setUTCHours(23, 59, 59, 999);
-          }
-          queryObject[field] = {
-            ...queryObject[field],
-            [operator]: castedFilterValue,
-          };
-        } else {
-          //how can I send two dates in my req.query
-          queryObject[field] = { [operator]: castedFilterValue };
-        }
-
-        from_fields.add(field);
+  const stringPaths = Object.keys(collection.schema.paths).filter(
+    (path) => collection.schema.paths[path].instance === "String"
+  );
+  const numberPaths = Object.keys(collection.schema.paths).filter(
+    (path) => collection.schema.paths[path].instance === "Number"
+  );
+  const numeric_query_arr = mapOperators(numericFilters).split(",");
+  // numericFilters = ["price>2000","rooms>2","createdAt>2023-04-01","createdAt<2023-04-10"]
+  // queryItem = ["price_$gt_2000","rooms_$gt_2","createdAt_$gt_2023-04-01","createdAt_$lt_2023-04-10"]
+  for (const queryItem of numeric_query_arr) {
+    // queryItem = "price_$gt_2000"
+    let [field, operator, filterValue] = queryItem.split("_");
+    if (field in numberPaths) {
+      if (field === "createdAt") {
+        operator === ">"
+          ? (filterValue = new Date(filterValue).setUTCHours(0, 0, 0, 0))
+          : (filterValue = new Date(filterValue).setUTCHours(23, 59, 59, 999));
+      } else {
+        filterValue = Number(filterValue);
       }
-    });
+      queryObject[field] = { [operator]: filterValue };
+    }
   }
 
-  if (idFields) {
-    for (const field of idFields) {
-      queryObject[field.fieldName] = field.id;
+  const jsonQuery = JSON.parse(query);
+  for (const key in jsonQuery) {
+    if (key in stringPaths) {
+      const regexPattern = new RegExp(jsonQuery[key], "i");
+      queryObject[key] = regexPattern;
     }
   }
 
@@ -93,18 +78,7 @@ export const buildQuery = async (
   return orderedResult;
 };
 
-function castNumericValue(field, filterValue) {
-  let castedFilterValue = filterValue;
-  if (field === "createdAt") {
-    castedFilterValue = new Date(castedFilterValue);
-    castedFilterValue.setUTCHours(0, 0, 0, 0);
-  } else if (field !== "createdAt") {
-    castedFilterValue = Number(castedFilterValue);
-  }
-  return castedFilterValue;
-}
-
-function mapOperators(numQuery) {
+function mapOperators(numericFilters) {
   const operatorMap = {
     ">": "$gt",
     ">=": "$gte",
@@ -113,9 +87,67 @@ function mapOperators(numQuery) {
     "<=": "$lte",
   };
   const filterOperator = /\b(<|<=|=|>=|>)\b/g;
-  let filters = numQuery.numericFilters.replace(
+  let filters = numericFilters.replace(
     filterOperator,
     (operator) => `_${operatorMap[operator]}_`
   );
   return filters;
 }
+
+/* 
+Give option for range queryObject.createdAt: { $gte: period.from, $lte: period.to }
+if (numQuery) {
+    let filters = mapOperators(numQuery);
+    const from_fields = new Set();
+    filters.split(",").forEach((item) => {
+      const [field, operator, filterValue] = item.split("_");
+      if (numQuery.options.includes(field)) {
+        let castedFilterValue = castNumericValue(field, filterValue);
+        // If appeard before
+        if (from_fields.has(field)) {
+          if (field === "createdAt") {
+            castedFilterValue.setUTCHours(23, 59, 59, 999);
+          }
+          queryObject[field] = {
+            ...queryObject[field],
+            [operator]: castedFilterValue,
+          };
+        } else {
+          //how can I send two dates in my req.query
+          queryObject[field] = { [operator]: castedFilterValue };
+        }
+        
+        from_fields.add(field);
+      }
+    });
+  }
+  */
+
+/*
+if (stringParams) {
+  stringParams.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+        console.log("[key, value]", [key, value]);
+        const match = key.match(/(.*)Options/);
+        if (match) {
+          const field = match[1];
+          queryObject[field] = { $regex: obj[field] };
+          if (key.endsWith("Options")) {
+            queryObject[field]["$options"] = value ? value : "";
+          }
+        }
+      });
+    });
+  }
+  */
+
+// function castNumericValue(field, filterValue) {
+//   let castedFilterValue = filterValue;
+//   if (field === "createdAt") {
+//     castedFilterValue = new Date(castedFilterValue);
+//     castedFilterValue.setUTCHours(0, 0, 0, 0);
+//   } else if (field !== "createdAt") {
+//     castedFilterValue = Number(castedFilterValue);
+//   }
+//   return castedFilterValue;
+// }
