549c94869d29c8b7b1e6e3c6a9a4c430f4004ad0
Maximiliano Castrejon <mcastrejon.dev.2021@gmail.com>
2024-07-13 14:21:10 -0600

    chore: Added libraries for functionality and added animations for components

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,6 +14,17 @@ module.exports = {
       gridTemplateRows: {
         // Simple 8 row grid
         7: 'repeat(7, minmax(0, 1fr))'
+      },
+
+      screens: {
+        tablet: '640px',
+        // => @media (min-width: 640px) { ... }
+
+        laptop: '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        desktop: '1280px'
+        // => @media (min-width: 1280px) { ... }
       }
     }
   },
