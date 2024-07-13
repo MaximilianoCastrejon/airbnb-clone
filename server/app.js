d4f3f222b37034c901406521b9fb53634bd544e6
Maximiliano Castrejon <mcastrejon.dev.2021@gmail.com>
2024-07-13 14:39:06 -0600

    chore: added routes to main app

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ import express from "express";
 import { errorHandler } from "./middleware/error-handler.middleware.js";
 import authRouter from "./routes/auth.routes.js";
 import listingRouter from "./routes/listing.routes.js";
+import draftRouter from "./routes/draft.listings.routes.js";
+import geoRouter from "./routes/geo.routes.js";
+import photoRouter from "./routes/photo.routes.js";
+import financeRouter from "./routes/finance.routes.js";
+import bookingRouter from "./routes/booking.routes.js";
+import discountRouter from "./routes/discount.routes.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
@@ -14,7 +20,13 @@ app.use(cors({ credentials: true, origin: "http://127.0.0.1:5173" }));
 app.use(cookieParser());
 
 app.use("/auth", authRouter);
+app.use("/photos", photoRouter);
 app.use("/listings", listingRouter);
+app.use("/bookings", bookingRouter);
+app.use("/draft", draftRouter);
+app.use("/finance", financeRouter);
+app.use("/geo", geoRouter);
+app.use("/discount", discountRouter);
 // app.use("/business", businessRouter);
 app.use(errorHandler);
 
